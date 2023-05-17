af757f58850075ce1cf147b816c93a11c6391022
Preslav Petkov <112109330+preslav30@users.noreply.github.com>
2023-05-17 20:26:48 +0300

    Create youtube_video_download.py

---
+++ youtube_video_download.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+
+
+def Download(link):
+    youtube_object = YouTube(link)
+    youtube_object = youtube_object.streams.get_highest_resolution()
+
+    download_location = input("Enter the download location (folder path): ") # D:\music
+
+    try:
+        youtube_object.download(download_location)
+        print("Download is completed successfully")
+    except:
+        print("An error has occurred")
+
+
+link = input("Enter the YouTube video URL: ")
+Download(link)
+print('Success')
